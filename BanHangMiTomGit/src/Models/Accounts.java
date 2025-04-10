ed86f95c4bebe460ac4fde2c7b477b1252e1bed8
thanhfptth03788 <thanhddth03788@fpt.edu.vn>
2025-04-10 08:31:19 +0700

    Update Accounts.java

--- BanHangMiTomGit/src/Models/Accounts.java
+++ BanHangMiTomGit/src/Models/Accounts.java
@@ -12,36 +12,28 @@ public class Accounts {
     private String matKhau;
     private String Email;
     private Integer chucVu;
-
     public Accounts() {
     }
-
     public Accounts(String matKhau, String Email, Integer chucVu) {
         this.matKhau = matKhau;
         this.Email = Email;
         this.chucVu = chucVu;
     }
-
     public String getMatKhau() {
         return matKhau;
     }
-
     public void setMatKhau(String matKhau) {
         this.matKhau = matKhau;
     }
-
     public String getEmail() {
         return Email;
     }
-
     public void setEmail(String Email) {
         this.Email = Email;
     }
-
     public Integer getChucVu() {
         return chucVu;
     }
-
     public void setChucVu(Integer chucVu) {
         this.chucVu = chucVu;
     }
