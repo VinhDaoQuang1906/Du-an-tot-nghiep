62713997523e86e6da73b3692504984a16cc0029
TLX2929 <tl255666@gmail.com>
2025-04-10 08:28:11 +0700

    Update Products.java

--- BanHangMiTomGit/src/Models/Products.java
+++ BanHangMiTomGit/src/Models/Products.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,6 +9,7 @@ package Models;
  *
  * @author jimbe
  */
+
 public class Products {
     private String ID;
     private String tenSP;
@@ -85,8 +87,5 @@ public class Products {
     public void setNgayThem(String ngayThem) {
         this.ngayThem = ngayThem;
     }
-
-
-    
     
 }
