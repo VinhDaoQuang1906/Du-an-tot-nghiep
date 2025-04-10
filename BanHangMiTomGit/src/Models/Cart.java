f79b45a109bcef5aa307ed131abb09dbf804e8c9
TLX2929 <tl255666@gmail.com>
2025-04-10 08:27:28 +0700

    Update Cart.java

--- BanHangMiTomGit/src/Models/Cart.java
+++ BanHangMiTomGit/src/Models/Cart.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,6 +9,7 @@ package Models;
  *
  * @author jimbe
  */
+
 public class Cart {
     private String product;
     private Integer amount;
