e5acb90eda9fc97434eae8780c143883c2831991
TLX2929 <tl255666@gmail.com>
2025-04-10 08:28:28 +0700

    Update Reviews.java

--- BanHangMiTomGit/src/Models/Reviews.java
+++ BanHangMiTomGit/src/Models/Reviews.java
@@ -1,13 +1,16 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package Models;
 
 /**
  *
  * @author jimbe
  */
+
 public class Reviews {
     private Integer soSao;
     private String NDDanhGia;
