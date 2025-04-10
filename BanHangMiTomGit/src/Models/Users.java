0f7962fbce9065e3296ab63c3bc18eeff72b9e60
TLX2929 <tl255666@gmail.com>
2025-04-10 08:28:48 +0700

    Update Users.java

--- BanHangMiTomGit/src/Models/Users.java
+++ BanHangMiTomGit/src/Models/Users.java
@@ -1,13 +1,16 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package Models;
 
 /**
  *
  * @author jimbe
  */
+
 public class Users {
     private String Email;
     private String loaiTK;
@@ -66,7 +69,5 @@ public class Users {
         this.diaChi = diaChi;
     }
 
-   
-    
 }
 
