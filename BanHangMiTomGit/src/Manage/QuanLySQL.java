8704dd16e2550647ea0d8f23b059f06097007c9b
VinhDaoQuang1906 <daoquangvinh1312@gmail.com>
2025-04-14 21:10:33 +0700

    Update QuanLySQL.java

--- BanHangMiTomGit/src/Manage/QuanLySQL.java
+++ BanHangMiTomGit/src/Manage/QuanLySQL.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package Manage;
 import Models.Accounts;
 import Models.Bills;
